381f72b498e52003775056816c922aa3b84d8f9e
ercubero <89366894+ercubero@users.noreply.github.com>
2021-10-02 08:27:56 -0700

    Update scripts.js
    deleted semicolon

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,4 @@ let pokemonList = [
   {name: "Ivysaur", height: 1, types: ['grass','poison']},
   {name: "Charizard", height: 1.1, types: ['fire','flying']},
   {name: "Weedle", height: 0.3, types: ['bug','poison']},
-];
+]
