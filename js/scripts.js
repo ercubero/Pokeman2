3e8a63a826311758c7e6f2cc95f7e0515d6ff0ac
ercubero <89366894+ercubero@users.noreply.github.com>
2021-11-07 16:02:45 -0800

    Update scripts.js
    Change format of list
    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 let pokemonList = [
-  {name: "Ivysaur", height: 1, types: ['grass','poison']},
-  {name: "Charizard", height: 1.1, types: ['fire','flying']},
-  {name: "Weedle", height: 0.3, types: ['bug','poison']},
-]
+  {
+    name: 'Ivysaur',
+    height: 1,
+    types: ['grass','poison']
+  },
+  {
+    name: 'Charizard',
+    height: "1.1",
+    types: ['fire','flying']
+  },
+  {
+    name: 'Weedle',
+    height: 0.3,
+    types: ['bug','poison']
+  },
+];
+
+for (let i=0; i < pokemonList.length; i++) {
+if (pokemonList[i].height > 1) {
+    document.write( pokemonList[i].name + ("\(Height: ") + pokemonList[i].height + '' ) "  + "-Wow that's big!" + '<br>' );
+  }
+else {
+    document.write( pokemonList[i].name + ("\(Height: ") + pokemonList[i].height + " ) " + "<br>" );
+  }
+}
