3a565e0a37e9fd45c6b64a5322db71053a54fc8a
ercubero <89366894+ercubero@users.noreply.github.com>
2021-11-09 23:15:59 -0800

    Update scripts.js
    adding a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,11 @@ let pokemonList = [
   },
 ];
 
+function myLoopFunction(pokemon) {
+  console.log(pokemon.name +  ("\(Height: ") + pokemon.height + ")" + pokemon.types);
+}
+pokemonList.forEach(myLoopFunction);
+
 for (let i=0; i < pokemonList.length; i++) {
 if (pokemonList[i].height > 1) {
     document.write( pokemonList[i].name + ("\(Height: ") + pokemonList[i].height + " ) "  + "-Wow that's big!" + '<br>' );
